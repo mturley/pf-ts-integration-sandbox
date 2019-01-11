e3c7d8922e7925c20b28a196d8e809ad172b5f1a
Mike Turley <mturley@redhat.com>
2019-01-11 14:26:01 -0500

    add puppeteer sanity check

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import puppeteer from 'puppeteer';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +8,15 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('loads properly in a browser', async () => {
+  let browser = await puppeteer.launch({
+    headless: true,
+  });
+  let page = await browser.newPage();
+
+  await page.goto('http://localhost:3030');
+  const found = (await page.content()).match(/Tooltip/gi)
+  expect(found).toBeTruthy();
+});
+
